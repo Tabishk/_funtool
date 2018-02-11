11eb6450a92d9bc3b45c30ff85c271c5295852bc
AAI-PC\AAI <varthamwar@gmail.com>
2018-02-12 00:13:01 +0530

    old project deleted

--- quickstart/src/app/app.module.js
+++ quickstart/src/app/app.module.js
@@ -7,7 +7,15 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
 };
 var core_1 = require("@angular/core");
 var platform_browser_1 = require("@angular/platform-browser");
-var my_component_1 = require("./my.component");
+var router_1 = require("@angular/router");
+var forms_1 = require("@angular/forms"); // mainly for banana in box two way data binding
+var http_1 = require("@angular/http"); // along with import we are registering http service with angulars injector
+// this auto work is done by HttpModule for us
+var app_component_1 = require("./app.component");
+var dashboardComponent_1 = require("./dashboardComponent");
+var LoginComponent_1 = require("./LoginComponent");
+var homeComponent_1 = require("./homeComponent");
+var RegistrationComponent_1 = require("./RegistrationComponent");
 var AppModule = (function () {
     function AppModule() {
     }
@@ -15,9 +23,18 @@ var AppModule = (function () {
 }());
 AppModule = __decorate([
     core_1.NgModule({
-        imports: [platform_browser_1.BrowserModule],
-        declarations: [my_component_1.MyComponent],
-        bootstrap: [my_component_1.MyComponent]
+        imports: [platform_browser_1.BrowserModule,
+            http_1.HttpModule,
+            forms_1.FormsModule,
+            router_1.RouterModule.forRoot([
+                { path: '', component: homeComponent_1.HomeComponent },
+                { path: 'dashboard', component: dashboardComponent_1.DashboardComponent },
+                { path: 'login', component: LoginComponent_1.LoginComponent },
+                { path: 'register', component: RegistrationComponent_1.RegistrationComponent },
+            ])
+        ],
+        declarations: [app_component_1.AppComponent, dashboardComponent_1.DashboardComponent, LoginComponent_1.LoginComponent, homeComponent_1.HomeComponent, RegistrationComponent_1.RegistrationComponent],
+        bootstrap: [app_component_1.AppComponent]
     })
 ], AppModule);
 exports.AppModule = AppModule;
--- quickstart/src/app/app.module.ts
+++ quickstart/src/app/app.module.ts
@@ -1,11 +1,27 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
-import { MyComponent }  from './my.component';
+import { RouterModule} from '@angular/router';
+import { FormsModule } from '@angular/forms'; // mainly for banana in box two way data binding
+import { HttpModule } from '@angular/http'; // along with import we are registering http service with angulars injector
+// this auto work is done by HttpModule for us
+import { AppComponent }  from './app.component';
+import { DashboardComponent } from './dashboardComponent';
+import { LoginComponent } from './LoginComponent';
+import { HomeComponent } from './homeComponent';
+import { RegistrationComponent } from './RegistrationComponent';
 
 @NgModule({
-  imports:      [ BrowserModule ],
-  declarations: [ MyComponent ],
-  bootstrap:    [ MyComponent ]
+  imports:      [ BrowserModule,
+                  HttpModule,
+                  FormsModule,
+                  RouterModule.forRoot([
+                    {path: '' , component: HomeComponent},
+                    {path: 'dashboard', component: DashboardComponent},
+                    {path: 'login', component: LoginComponent},
+                    {path: 'register', component: RegistrationComponent},
+                  ])
+                ], // to available this module in our application
+  declarations: [ AppComponent, DashboardComponent, LoginComponent, HomeComponent,RegistrationComponent],
+  bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
